7258d740a106906ce6e2d17f0543d80f3d05967c
aaaasif <alexasif4@gmail.com>
2021-08-07 03:23:57 +0600

    leapYearMathAdded

--- leap-year.js
+++ leap-year.js
@@ -14,3 +14,19 @@ console.log('Is my year leap year', isMyYearLeapYear);
 const yourYear = 2224;
 const isYourYearLeapYear = isLeapYear(yourYear);
 console.log('is your year leap year', isYourYearLeapYear);
+
+// the right way to get Leap Year
+
+function leapYear(year){
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
+
+const year = 2100;
+const isMyYeaarLeapYear = leapYear(year);
+console.log('Is my year leap year?', isMyYeaarLeapYear)
